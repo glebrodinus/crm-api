086988d2f0592ec6135f98c0f9575dcfef5c8ceb
Gleb Rodin <131303394+glebrodinus@users.noreply.github.com>
2025-12-26 20:09:29 -0800

    updating migrations

---
+++ database/migrations/2025_12_27_035609_create_verification_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('verification_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('identifier'); // Email or phone number
+            $table->string('type'); // email | phone
+            $table->mediumInteger('code');
+            $table->timestamp('expires_at')->index();
+            $table->timestamps();
+            
+            // Only one active token per identifier + type
+            $table->unique(['identifier', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('verification_tokens');
+    }
+};
