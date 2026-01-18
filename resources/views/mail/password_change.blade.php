c76483768c95122b56f0fabe7bad03f04f276774
Gleb Rodin <131303394+glebrodinus@users.noreply.github.com>
2026-01-18 14:16:26 -0800

    adding email blades

---
+++ resources/views/mail/password_change.blade.php
@@ -0,0 +1,6 @@
+Your password was successfully changed on {{ $timestamp }}.
+
+If you did not make this change, please reset your password immediately or contact support.
+
+Thank you,
+Support Team
