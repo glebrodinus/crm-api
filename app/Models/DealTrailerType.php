95404a7212db67deceb8d73c6939ea7fbad502a0
Gleb Rodin <131303394+glebrodinus@users.noreply.github.com>
2026-03-05 17:40:09 -0800

    updating trailer types

--- app/Models/DealTrailerType.php
+++ app/Models/DealTrailerType.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DealTrailerType extends Model
 {
-    //
+    protected $fillable = [
+        'deal_id',
+        'type',
+    ];
+
+    public function deal(): BelongsTo
+    {
+        return $this->belongsTo(Deal::class);
+    }
 }
