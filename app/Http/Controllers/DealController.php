a2bcdb0b7e3ebee99f5fbd1244a1c5b52bae8282
Gleb Rodin <rodin.gleb@gmail.com>
2026-03-13 07:27:06 -0700

    uopdating deal

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -73,6 +73,9 @@ class DealController extends Controller
             'is_hazardous' => ['nullable', 'boolean'],
             'is_driver_assist_required' => ['nullable', 'boolean'],
             'is_ramps_required' => ['nullable', 'boolean'],
+            'is_twic_required' => ['nullable', 'boolean'],
+            'is_emodal_required' => ['nullable', 'boolean'],
+            'is_bonded_carrier_required' => ['nullable', 'boolean'],
 
             'is_temp_required' => ['nullable', 'boolean'],
             'temperature_from' => ['nullable', 'integer', 'min:-100', 'max:150'],
@@ -238,6 +241,9 @@ class DealController extends Controller
             'is_hazardous' => ['nullable', 'boolean'],
             'is_driver_assist_required' => ['nullable', 'boolean'],
             'is_ramps_required' => ['nullable', 'boolean'],
+            'is_twic_required' => ['nullable', 'boolean'],
+            'is_emodal_required' => ['nullable', 'boolean'],
+            'is_bonded_carrier_required' => ['nullable', 'boolean'],
 
             'is_temp_required' => ['nullable', 'boolean'],
             'temperature_from' => ['nullable', 'integer', 'min:-100', 'max:150'],
